1fdc018702d77e7a67480f402d856f4450ba0190
GavdzinskyiVeacheslav <veacheslavgavdzynsky@gmail.com>
2021-09-07 22:12:44 +0300

    Added example - TGbot_Weather.YouTube

---
+++ TGbot_Weather.YouTube.py
@@ -0,0 +1,31 @@
+import telebot
+from pyowm import OWM
+from pyowm.utils.config import get_default_config
+config_dict = get_default_config()
+config_dict['language'] = 'ru'
+
+owm = OWM('2fb7b73a709c0fb8d670c52d9f3e199b', config_dict)
+mgr = owm.weather_manager()
+bot = telebot.TeleBot("1992611247:AAFKqBQmzkoQThkVaITEGxnRGDdWGx_9f3M")
+
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+    observation = mgr.weather_at_place(message.text)
+    w = observation.weather
+    temp = w.temperature('celsius')["temp"]
+
+    answer = "В городе " + message.text + " сейчас " + w.detailed_status + "\n"
+    answer += "Температура в районе " + str(temp) + "\n\n"
+
+    if temp < 10:
+        answer += "Сейчас очень холодно, одевайся как танк!"
+    elif temp < 20:
+        answer += "Холодно, одевайся потеплее!"
+    else:
+        answer += "Температура норм, одевай что угодно"
+
+    bot.send_message(message.chat.id, answer)
+
+
+bot.polling(none_stop=True)
