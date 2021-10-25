41222cc450f992b4be9dcd5f3b3a002f1f8e19c9
GavdzinskyiVeacheslav <veacheslavgavdzynsky@gmail.com>
2021-10-25 16:59:06 +0300

    Revision: 1. Set up normal variable names 2. It turned out to make one more helper function 3. Improved docstring explanation
    --In the next commit, translate them into English--

--- Working_with_files(txt)1.py
+++ Working_with_files(txt)1.py
@@ -1,32 +1,39 @@
-
 """
-Программа читает с файла построчный список с оценками из 4-ёх элементов,
-выводит среднюю оценку строки и столбца. Округляет ответ(float) до 9 знака после запятой
+Программа читает с файла построчный список с оценками из 4-ёх элементов(Имя студента и три оценки).
+Выводит среднюю оценку строки и всех столбцов по первой, второй и третей оценке.
+Округляет ответ в(float) до 9 знака после запятой.
 """
 
-count_s, sum_gr, sum_gr1, sum_gr2, sum_gr3, = 0, 0, [], [], []
+count_students, sum_grade_str = 0, 0
+sum_grade_column1, sum_grade_column2, sum_grade_column3 = [], [], []
 
-c = open("dataset_3363_4 (1).txt", "r", encoding='utf-8')
+text = open("Test file(1).txt", "r", encoding='utf-8')
+
+
+def add_indices_to_arr(lis_t: list, index: str):
+    """ Эта функция - создаёт список чисел столбца по индексам строки.
+    Например все первые или все вторые элементы строки """
+    return lis_t.append(int(index))
+
+
+def average(x: list):
+    """ Эта функция - вычисляет среднее арифметческое оценок столбцов """
+    return round(sum(x) / count_students, 9)
+
 
 while True:
-    s = c.read().split()
+    work_space = text.read().split()
 
-    if not s:
+    if not work_space:
         break
 
-    for line in s:
-        count_s += 1
+    for line in work_space:
+        count_students += 1  # number of lines in the file(number of students)
         line = line.strip().split(';')
-        sum_gr = (int(line[1]) + int(line[2]) + int(line[3])) / (len(line) - 1)
-        sum_gr1.append(int(line[1]))
-        sum_gr2.append(int(line[2]))
-        sum_gr3.append(int(line[3]))
-        print(round(sum_gr, 9))
+        sum_grade_str = (int(line[1]) + int(line[2]) + int(line[3])) / (len(line) - 1)
+        add_indices_to_arr(sum_grade_column1, line[1])
+        add_indices_to_arr(sum_grade_column2, line[2])
+        add_indices_to_arr(sum_grade_column3, line[3])
+        print(round(sum_grade_str, 9))
 
-
-def f(x: list):
-    return round(sum(x) / count_s, 9)
-
-
-print(f(sum_gr1), f(sum_gr2), f(sum_gr3))
-
+print(average(sum_grade_column1), average(sum_grade_column2), average(sum_grade_column3))
